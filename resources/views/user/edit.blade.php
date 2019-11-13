3de56393d21ac8a0358fb096617b6945777945e0
Cadmiel Matioli Donato <cadmatioli@gmail.com>
2019-11-13 09:20:11 -0300

    added items on user edit

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -34,7 +34,7 @@
                                             <input type="email" class="form-control" id="email" name="email" placeholder="email" value="{{auth()->user()->email}}">
                                         </div>
                                         <div class="col-md-12">
-                                            <label for="age">Endereço de Email</label>
+                                            <label for="age">Idade</label>
                                             <input type="age" class="form-control" id="age" name="age" placeholder="idade" value="{{auth()->user()->age}}">
                                         </div>
                                         <div class="col-md-12" align="center">
@@ -55,9 +55,11 @@
                                 <div class="icon"><i class="ion-ios-list-outline"></i></div>
                             </div>
                             <h2 class="title"><a href="#">O que Procura?</a></h2>
-                            <p>
-                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem  doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
-                            </p>
+                            <div class="col-md-12">
+                                <label for="age">Idade</label>
+                                <input type="age" class="form-control" id="age" name="age" placeholder="idade" value="{{auth()->user()->age}}">
+                                <br>
+                            </div>
                         </div>
                     </div>
                     
@@ -68,14 +70,14 @@
                                 <div class="icon"><i class="ion-ios-eye-outline"></i></div>
                             </div>
                             <h2 class="title"><a href="#">Como você se vê no futuro?</a></h2>
-                            <p>
-                                Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores eos qui ratione voluptatem sequi nesciunt Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
-                            </p>
+                            <div class="col-md-12">
+                                <label for="age">Idade</label>
+                                <input type="age" class="form-control" id="age" name="age" placeholder="idade" value="{{auth()->user()->age}}">
+                                <br>
+                            </div>
                         </div>
                     </div>
-                    
                 </div>
-                
             </div>
         </section>
         <!-- #about -->
@@ -127,16 +129,6 @@
             </div>
         </section>
         <!-- #services -->
-                    
-        <!--==========================Call To Action Section============================-->
-        <section id="call-to-action" class="wow fadeIn">
-            <div class="container text-center">
-                <h3>Call To Action</h3>
-                <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <a class="cta-btn" href="#">Call To Action</a>
-            </div>
-        </section>
-        <!-- #call-to-action -->
                         
         <!--==========================Skills Section============================-->
         <section id="skills">
